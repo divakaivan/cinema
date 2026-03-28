ff533a882e67764a62fb4480fac7dd8bcc31a573
divakaivan <timee98@outlook.com>
2026-03-28 18:41:50 +0000

    serve basic http page

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"github.com/divakaivan/cinema/internal/utils"
+)
 
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /movies", listMovies)
+
+	mux.Handle("GET /", http.FileServer(http.Dir("static")))
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
+
+var movies = []movieResponse{
+	{ID: "inception", Title: "Inception", Rows: 5, SeatsPerRow: 8},
+	{ID: "dune", Title: "Dune: Part Two", Rows: 4, SeatsPerRow: 6},
+}
+
+func listMovies(w http.ResponseWriter, r *http.Request) {
+	utils.WriteJSON(w, http.StatusOK, movies)
+}
+
+type movieResponse struct {
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Rows int `json:"rows"`
+	SeatsPerRow int `json:"seats_per_row"`
 }
